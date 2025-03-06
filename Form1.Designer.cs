488ef61fd01050505b2beba4ff90ff46c8f15aad
Samuel <samuel.saxgarn@gmail.com>
2025-03-06 21:14:23 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Online_store
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnFetch = new System.Windows.Forms.Button();
+            this.listBoxMain = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(323, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(112, 30);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open connection";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(323, 48);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(112, 32);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close connection";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnFetch
+            // 
+            this.btnFetch.Location = new System.Drawing.Point(12, 19);
+            this.btnFetch.Name = "btnFetch";
+            this.btnFetch.Size = new System.Drawing.Size(75, 23);
+            this.btnFetch.TabIndex = 2;
+            this.btnFetch.Text = "fetch data";
+            this.btnFetch.UseVisualStyleBackColor = true;
+            this.btnFetch.Click += new System.EventHandler(this.btnFetch_Click);
+            // 
+            // listBoxMain
+            // 
+            this.listBoxMain.FormattingEnabled = true;
+            this.listBoxMain.Location = new System.Drawing.Point(12, 68);
+            this.listBoxMain.Name = "listBoxMain";
+            this.listBoxMain.Size = new System.Drawing.Size(235, 199);
+            this.listBoxMain.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 451);
+            this.Controls.Add(this.listBoxMain);
+            this.Controls.Add(this.btnFetch);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Online store aq3187";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnFetch;
+        private System.Windows.Forms.ListBox listBoxMain;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace Online_store
+{
+    public partial class Form1 : Form
+    {
+
+        private NpgsqlConnection _conn; //för hålla anslutningen till databasen
+        private string connString = ""; //min login
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (_conn != null && _conn.State == System.Data.ConnectionState.Open)//om den finns och är öppen
+            {
+                _conn.Close();  // Stänger
+                MessageBox.Show("Connection closed successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No open connection to close.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+
+            _conn = new NpgsqlConnection(connString);  // Skapar anslutning
+
+            try
+            {
+                _conn.Open();  // Öppnar anslutningen
+                MessageBox.Show("Connection to PostgreSQL successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Connection failed: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnFetch_Click(object sender, EventArgs e)
+        {
+            if (_conn != null && _conn.State == System.Data.ConnectionState.Open)
+            {
+                try
+                {
+                    string query = "SELECT * FROM customer";  // SQL-fråga för att hämta alla kunder
+                    using (var cmd = new NpgsqlCommand(query, _conn))
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        listBoxMain.Items.Clear();  // Tömmer listan innan nya data läggs till
+
+                        while (reader.Read())
+                        {
+                            // Lägg till information från varje rad i ListBox
+                            string customerInfo = $"ID: {reader["id"]}, Name: {reader["firstname"]}, lastname: {reader["lastname"]}";
+                            listBoxMain.Items.Add(customerInfo);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error fetching data: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No open connection to the database.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
